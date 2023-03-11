9bb27aafc2e42b47f3e6f9dc841522758a8bce39
liza <liza@lizas-MacBook-Air.local>
2023-03-11 14:21:06 +0300

    add changes to cart - delete button

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -37,10 +37,17 @@ function CartPage () {
                       count: elem.count - 1,
                       totalPrice: elem.totalPrice - elem.actualPrice,
                     }
+                    
                   }
                   return elem;
+                  
                 }
-                )})
+            )})
+
+            if(item.count === 1) {
+                deleteFromCart(item)
+            } 
+            
         } 
         console.log(cart)
     }
@@ -91,14 +98,12 @@ function CartPage () {
                                     <path d="M16 8L8 16M8.00001 8L16 16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#C9C9C9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                                 </svg>
                                 
-
-                               
                                 </div>
                             <p className="cartPage-tile__price">{item.totalPrice} ₽</p>
-                            {/* <svg onClick={() => deleteFromCart(item)}
+                            <svg className="cartPage-tile-amount__button" onClick={() => deleteFromCart(item)}
                              width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M16 8L8 16M8.00001 8L16 16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#C9C9C9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg> */}
+                            </svg>
                             
                             
                         </div>
